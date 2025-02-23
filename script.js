e6c226acba2f3da3d9956943cef31ce66adc6533
isthathowitworks <ianwho721@gmail.com>
2025-02-23 20:53:57 +0800

    Update and rename javascript.js to script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ function check() {
     document.getElementById("result").innerText = num % 2 === 0 ? "Even" : "Odd";
 }
 
-// Print numbers from 1 to 10 in console
+// console of 1-10
 for (let i = 1; i <= 10; i++) console.log(i);
